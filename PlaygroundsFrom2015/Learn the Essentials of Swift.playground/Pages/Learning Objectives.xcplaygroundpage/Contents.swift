a3d9f94bff4d4a0de0dbd75fe2a63dea3f49f98e
T. Andrew Binkowski <andrew@babybinks.com>
2016-04-13 10:47:05 -0500

    Add the 2015 Playgrounds.

---
+++ PlaygroundsFrom2015/Learn the Essentials of Swift.playground/Pages/Learning Objectives.xcplaygroundpage/Contents.swift
@@ -0,0 +1,35 @@
+//: # Learn the Essentials of Swift
+//:
+//: Your first lesson is presented in the form of a guided Swift _playground_, a type of file that lets you change and interact with the code directly in Xcode and see the result immediately. Playgrounds are great for learning and experimenting, and this one helps get you up to speed on fundamental Swift concepts.
+//:
+//: ## Learning Objectives
+//:
+//: At the end of the lesson, you’ll be able to:
+//:
+//: * Differentiate between a constant and a variable
+//:
+//: * Know when to use implicit and when to use explicit type declarations
+//:
+//: * Understand the advantage of using optionals and optional binding
+//:
+//: * Differentiate between optionals and implicitly unwrapped optionals
+//:
+//: * Understand the purpose of conditional statements and loops
+//:
+//: * Use `switch` statements for conditional branching beyond a binary condition
+//:
+//: * Use `where` clauses to impose additional constraints in conditional statements
+//:
+//: * Differentiate between functions, methods, and initializers
+//:
+//: * Differentiate between classes, structures, and enumerations
+//:
+//: * Understand syntax for (and basic concepts behind) inheritance and protocol conformance
+//:
+//: * Determine implicit types and find additional information using Xcode’s quick help shortcut (Option-click)
+//:
+//: * Import and use UIKit
+//:
+//: See [License](License) for this sample's licensing information.
+//: 
+//: [Next](@next)
